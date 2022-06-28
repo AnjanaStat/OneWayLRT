373931b4030d92ebe5151e91d5730970dbe4cb49
AnjanaStat <107476002+AnjanaStat@users.noreply.github.com>
2022-06-28 13:09:47 +0530

    Initial commit

---
+++ R/OneWayLRT.R
@@ -0,0 +1,11 @@
+#' Find test statistic value from a given data and corresponding critical value
+#'
+#' This package finds critical value along with statistic value of bootstrap based LRT method for testing equility of
+#' treatment effects against ordered alternative in One-Way ANOVA model with heteroscedasticity.
+#'
+#' @docType package
+#'
+#' @author Anjana Mondal \email{mondal.anjana25@gmail.com}
+#'
+#' @name OwoWayLRT
+NULL
